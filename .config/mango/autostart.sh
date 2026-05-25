7eeb5c2a259de08b2ce162084c700e0c02bea895
Tomáš Kubeček <tkubecek@trask.cz>
2026-05-25 13:07:48 +0200

    Updated dotfiles

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 swaybg -i ~/Media/Wallpapers/ultrakill.png -m fill &
-swayidle -w \
-	timeout 300 'swaylock' \
-	before-sleep 'swaylock' &
+
+swayidle -w timeout 300 'swaylock' &
+
+wl-clip-persist --clipboard regular --reconnect-tries &
+
 mako &
+
 kanshi &
+
 waybar &
 
